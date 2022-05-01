2eb29a7b5451a6dcd305f1e92991402a59782589
PankajKarki <pankaj.karki.786@gmail.com>
2022-05-01 23:21:21 +0530

    added fastapi

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ install:
 		pip install -r requirements.txt
 format:
 	# format code
-	black *.py mylib/*.py
+	black *.py mylib/*.py tests/*.py
 lint:
 	# flake8, pylint
-	pylint --disable=R,C *.py mylib/*.py
+	pylint --disable=R,C *.py mylib/*.py tests/*.py
 test:
 	# test
-	python -m pytest -vv --cov=mylib tests/test_logic.py
+	python -m pytest -vv --cov=tests tests/
 deploy:
 	# deploy
 all: install format lint test deploy
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,4 +1,5 @@
 import wikipedia
+from textblob import TextBlob
 
 
 def wiki(name="War Goddess", length=1):
@@ -9,10 +10,21 @@ def wiki(name="War Goddess", length=1):
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
 
-def search(name):
+
+def search_wiki(name):
     """
     Search wikipedia names
     """
 
     results = wikipedia.search(name)
     return results
+
+
+def phrase(name):
+    """
+    Return phrase from wikipedia
+    """
+
+    page = wiki(name)
+    blob = TextBlob(page)
+    return blob.noun_phrases
